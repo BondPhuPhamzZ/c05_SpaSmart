bf2049ad7cb490504d2dc5e563108f808b49bdd6
BondSensei <phuphamwork89@gmail.com>
2026-06-30 21:52:01 +0700

    Xóa toàn bộ để cập nhật mới

--- c05_SpaSmart/Models/Enums/UserRole.cs
+++
@@ -1,8 +0,0 @@
-namespace c05_SpaSmart.Models.Enums
-{
-    public enum UserRole
-    {
-        Admin = 1,
-        Customer = 2
-    }
-}
--- c05_SpaSmart/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace c05_SpaSmart.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
