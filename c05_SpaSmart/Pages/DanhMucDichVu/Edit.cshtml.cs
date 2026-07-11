4852a7d4057cd0db97e215c7b968322923120ed9
BondSensei <phuphamwork89@gmail.com>
2026-07-11 22:02:28 +0700

    feat(Admin): Implement Quản lý Danh mục loại module

---
+++ c05_SpaSmart/Pages/DanhMucDichVu/Edit.cshtml.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using c05_SpaSmart.Models;
+
+namespace c05_SpaSmart.Pages.DanhMucDichVu
+{
+    public class EditModel : PageModel
+    {
+        private readonly C05SpaSmartContext _context;
+
+        public EditModel(C05SpaSmartContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public c05_SpaSmart.Models.DanhMucDichVu DanhMuc { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.DanhMucDichVus == null)
+            {
+                return NotFound();
+            }
+
+            var danhmuc =  await _context.DanhMucDichVus.FirstOrDefaultAsync(m => m.Id == id);
+            if (danhmuc == null)
+            {
+                return NotFound();
+            }
+            DanhMuc = danhmuc;
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(DanhMuc).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DanhMucExists(DanhMuc.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool DanhMucExists(int id)
+        {
+          return (_context.DanhMucDichVus?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ c05_SpaSmart/Pages/DanhMucDichVu/Index.cshtml.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using c05_SpaSmart.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace c05_SpaSmart.Pages.DanhMucDichVu
+{
+    public class IndexModel : PageModel
+    {
+        private readonly C05SpaSmartContext _context;
+
+        public IndexModel(C05SpaSmartContext context)
+        {
+            _context = context;
+        }
+
+        public IList<c05_SpaSmart.Models.DanhMucDichVu> DanhMucDichVus { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.DanhMucDichVus != null)
+            {
+                DanhMucDichVus = await _context.DanhMucDichVus.ToListAsync();
+            }
+        }
+
+        public async Task<IActionResult> OnPostDeleteAsync(int id)
+        {
+            var danhmuc = await _context.DanhMucDichVus.FindAsync(id);
+            if (danhmuc != null)
+            {
+                _context.DanhMucDichVus.Remove(danhmuc);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+    }
+}
